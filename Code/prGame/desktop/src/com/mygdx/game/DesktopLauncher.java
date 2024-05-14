610864cc9198a9beb2eb70d84692411c49a61050
PabloHorJim <pablohorjim@uma.es>
2024-05-14 21:07:50 +0200

    Added documentation for Desktop Launcher
    Read and understood the importance of DesktopLauncher.java class, added a detailed documentation on each section of the code so that every group memeber can understand it easily

--- Code/prGame/desktop/src/com/mygdx/game/DesktopLauncher.java
+++ Code/prGame/desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -4,12 +4,32 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.mygdx.game.MyGdxGame;
 
-// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+/**
+* This class is the entry point of the game, it contains the main method, which will be executed when launching
+* the game.
+*/
 public class DesktopLauncher {
+
+	//The frames per second at which the game will run.
+	public static final int FRAMES_PER_SECOND = 30; //NFR002
+	//Title of the game.
+	public static final String GAME_TITLE = "Dragon_Boat_Race";
+
+	/**
+	* The main method which is the entry point of the Java application.
+	* @param arg The command line arguments passed to the application.
+	*/
 	public static void main (String[] arg) {
+		// Create a new configuration object for the application
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setForegroundFPS(60);
-		config.setTitle("Dragon_Boat_Race");
+
+		// Set the foreground frames per second for the application
+		config.setForegroundFPS(FRAMES_PER_SECOND);
+
+		// Set the title of the application window
+		config.setTitle(GAME_TITLE);
+
+		// Create a new application instance and start the game loop
 		new Lwjgl3Application(new MyGdxGame(), config);
 	}
 }
