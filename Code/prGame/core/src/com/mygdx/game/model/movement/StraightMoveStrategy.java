97a0742a8796b3af674fed4a195a27ad84c4c0c9
PabloHorJim <pablohorjim@uma.es>
2024-05-17 19:29:57 +0200

    Fix classes relative to movement Strategies
    Changed MovementStrategy interface so that the move method it enforces receives a Movable object and a float delta(time elapsed).
    Changed all Strategies to adapt to the change acordinly

--- Code/prGame/core/src/com/mygdx/game/model/movement/StraightMoveStrategy.java
+++ Code/prGame/core/src/com/mygdx/game/model/movement/StraightMoveStrategy.java
@@ -2,7 +2,7 @@ package com.mygdx.game.model.movement;
 
 public class StraightMoveStrategy implements MovementStrategy {
     @Override
-    public void move(float delta) {
+    public void move(Movable movable, float delta) {
         //TODO
     }
 }
