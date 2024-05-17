77b1d89ba29624a091eaac6ddc4c52f29022bded
PabloHorJim <pablohorjim@uma.es>
2024-05-17 19:59:14 +0200

    Fixed powerup implementation
    Changed the powerUps implementation to be responsible for their effect.

--- Code/prGame/core/src/com/mygdx/game/model/PowerUp.java
+++ Code/prGame/core/src/com/mygdx/game/model/PowerUp.java
@@ -1,13 +1,35 @@
+package com.mygdx.game.model;
+
 public class PowerUp {
-    //A powerUp has a powerUpType which is a enum that represents the type of powerUp we are colliding
-    private PowerUpType powerUp;
+    private final int HEALTH_INCREASE = 100;
+    private final int SPEED_INCREASE = 2;
+    //A powerUp has a powerUpType which is an enum that represents the type of powerUp we are colliding
+    private final PowerUpType type;
     //The constructor for the powerUp class
-    public PowerUp(PowerUpType powerUp){
-        this.powerUp = powerUp;
+    public PowerUp(PowerUpType type){
+        this.type = type;
     }
     //Getter for the powerUpType
     public PowerUpType getPowerUpType(){
-        return powerUp; //return the powerUpType
+        return type;
+    }
+    public void applyPowerUp(Boat boat){
+        //Switch case for the powerUpType
+        switch (type){
+            case HEALTH:
+                //Increase the health of the boat
+                boat.adjustHealth(HEALTH_INCREASE);
+                break;
+            case SPEED:
+                //Increase the speed of the boat
+                boat.adjustSpeed(SPEED_INCREASE);
+                break;
+            case INVINCIBLE:
+                //Make the boat invincible
+                boat.setInvincible(true);
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid power up type");
+        }
     }
 }
-
--- Code/prGame/core/src/com/mygdx/game/model/PowerUpType.java
+++ Code/prGame/core/src/com/mygdx/game/model/PowerUpType.java
@@ -1,4 +1,8 @@
+package com.mygdx.game.model;
+
 public enum PowerUpType {
     //Types of powerUps that can be obtained
-    HEALTH,SPEED,INVENCIBILITY;
+    HEALTH,
+    SPEED,
+    INVINCIBLE
 }
