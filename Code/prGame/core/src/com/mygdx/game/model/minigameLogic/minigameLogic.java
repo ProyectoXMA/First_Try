5470e3502e6819022d3a49f128bd063f14c7abf3
Angel Escaño <byanicoyt@gmail.com>
2024-05-24 15:53:06 +0200

    Added class minigameLogic
    To make sure that the minigameScreen only does things related to the screen, i have inplemented the class minigameLogic where all the functionality are descripted

---
+++ Code/prGame/core/src/com/mygdx/game/model/minigameLogic/minigameLogic.java
@@ -0,0 +1,132 @@
+package com.mygdx.game.model.minigameLogic;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputAdapter;
+import com.mygdx.game.GameState;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class minigameLogic {
+    private GameState gameState;
+    private int successCounter;
+    private int failCounter;
+    private String currentWord;
+    private String typedWord;
+    private double startTime;
+    private double timeLimit;
+    private double remainingTime;
+    private int r;
+    private List<String> words;
+
+    public minigameLogic(GameState gameState) {
+        this.gameState = gameState;
+        successCounter = 0;
+        failCounter = 0;
+        this.words = Arrays.asList("Dragon", "Boat", "Racing", "Duck", "Ancient", "Ritualistic", "China", "Competition", "River", "Tradition");
+        typedWord = "";
+        timeLimit = 10000;
+    }
+
+    public double getRemainingTime() {
+        return remainingTime;
+    }
+
+    public double getTimeLimit() {
+        return timeLimit;
+    }
+
+    public int getFailCounter() {
+        return failCounter;
+    }
+
+    public List<String> getWords() {
+        return words;
+    }
+
+    public String getTypedWord() {
+        return typedWord;
+    }
+
+    public GameState getGameState() {
+        return gameState;
+    }
+
+    public String getCurrentWord() {
+        return currentWord;
+    }
+
+    // Method to read Player input
+    //
+    // @param character stores the key pressed by the user
+    // @method checkWord() process the keystrokes stored in character after 'Enter' key is pressed
+    public void generateAdapter(){
+        Gdx.input.setInputProcessor(new InputAdapter(){
+            @Override
+            public boolean keyTyped(char character) {
+                // Check word after 'Enter' is pressed (carriage return or newline button)
+                if(character == '\r' || character == '\n') {
+                    checkWord();
+                } else { // Any other key other than 'Enter' is pressed
+                    typedWord = typedWord + character; // Register such key on the string
+                }
+
+                // If the 'Backspace' button is pressed nothing has to be done
+                if(character == '\b'){
+
+                }
+
+                return true;
+            }
+        });
+        // @method to show the next word to type
+        // @var startTime is restarted on every the word call
+        generateNewWord();
+        startTime = System.currentTimeMillis();
+    }
+    // Method to display the next word to the Player
+    //
+    // @var currentWord will take the vale of any word from the predefined words list
+    // @var typedWord is reseted on every function call
+    public void generateNewWord(){
+        r = new Random().nextInt(10);
+        currentWord = words.get(r);
+        typedWord = "";
+    }
+    // Method to verify typed words read from Player input
+    //
+    public void checkWord(){
+        // Verify that typed word is the one showed in the panel
+        if (typedWord.equals(currentWord)) {
+            successCounter++;
+        } else {
+            failCounter++;
+        }
+        // After each word check verify if success or fail conditions are met
+        checkGameState();
+        // Then if needed, show next word
+        generateNewWord();
+        startTime = System.currentTimeMillis();
+    }
+
+    public int checkGameState(){
+        if(successCounter == 1){
+            return successCounter;
+        }else if(failCounter == 3){
+            return failCounter;
+        }
+        return 0;
+    }
+
+    public void run(){
+        // Calculates the remaining time to display real time coundown
+        remainingTime = (timeLimit - (System.currentTimeMillis() - startTime)) / 1000f;
+        if(remainingTime <= 0){
+            failCounter++;
+            checkGameState();
+            generateNewWord();
+            startTime = System.currentTimeMillis();
+        }
+    }
+}
--- Code/prGame/core/src/com/mygdx/game/view/MinigameScreen.java
+++ Code/prGame/core/src/com/mygdx/game/view/MinigameScreen.java
@@ -16,7 +16,7 @@ import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.game.GameState;
 import com.mygdx.game.MyGdxGame;
-
+import com.mygdx.game.model.minigameLogic.minigameLogic;
 
 
 public class MinigameScreen implements Screen {
@@ -29,15 +29,7 @@ public class MinigameScreen implements Screen {
 // attributes for the minigame
     private final MyGdxGame game;
     private GameState gameState;
-    private java.util.List<String> words;
-    private int successCounter = 0;
-    private int failCounter = 0;
-    private String currentWord;
-    private String typedWord = "";
-    private double startTime;
-    private double timeLimit;
-    private double remainingTime;
-    private int r = new Random().nextInt(10);
+    private minigameLogic minigameLogic;
     // Dimensions of assets
     private final int panelHeight = 180;
     private final int panelWidth = 300;
@@ -55,12 +47,12 @@ public class MinigameScreen implements Screen {
     public MinigameScreen(final MyGdxGame game, GameState gameState) {
         this.game = game;
         this.gameState = gameState;
-        this.words = Arrays.asList("Dragon", "Boat", "Racing", "Duck", "Ancient", "Ritualistic", "China", "Competition", "River", "Tradition");
-        this.timeLimit = 10000; // 10 seconds to type the word
-        epicMusic = new 
+        epicMusic = Gdx.audio.newMusic(Gdx.files.internal("epicMusic.mp3"));
+        epicMusic.setLooping(true);
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 800, 480);
-        
+        this.minigameLogic = new minigameLogic(gameState);
+
         backgroundImage = new Texture(Gdx.files.internal("possible_background.png"));
         textOutput = new Texture(Gdx.files.internal("textPanel2.png"));
         playerInput = new Texture(Gdx.files.internal("textPanel2.png"));
@@ -78,80 +70,8 @@ public class MinigameScreen implements Screen {
         viewport = new ExtendViewport(800, 480);
         // stage displays all the actors involved in the screen (UI, buttons, labels, etc)
         stage = new Stage(viewport);
-
-        // Method to read Player input
-        //
-        // @param character stores the key pressed by the user
-        // @method checkWord() process the keystrokes stored in character after 'Enter' key is pressed
-        Gdx.input.setInputProcessor(new InputAdapter() {
-            @Override
-            public boolean keyTyped(char character) {
-                // Check word after 'Enter' is pressed (carriage return or newline button)
-                if(character == '\r' || character == '\n') {
-                    checkWord();
-                } else { // Any other key other than 'Enter' is pressed
-                    typedWord = typedWord + character; // Register such key on the string
-                }
-
-                // If the 'Backspace' button is pressed nothing has to be done
-                if(character == '\b'){
-                    //TODO avoid visual bugs on execution
-                }
-
-                return true;
-            }
-        });
-
-        // @method to show the next word to type
-        // @var startTime is restarted on every the word call
-        generateNewWord();
-        startTime = System.currentTimeMillis();
-
+        minigameLogic.generateAdapter();
     }
-
-        // Method to display the next word to the Player
-        //
-        // @var currentWord will take the vale of any word from the predefined words list
-        // @var typedWord is reseted on every function call
-        private void generateNewWord() {
-            r = new Random().nextInt(10);
-            currentWord = words.get(r);
-            typedWord = "";
-        }
-
-        // Method to verify typed words read from Player input
-        //
-        private void checkWord() {
-            // Verify that typed word is the one showed in the panel
-            if (typedWord.equals(currentWord)) {
-                successCounter++;
-            } else {
-                failCounter++;
-            }
-            // After each word check verify if success or fail conditions are met
-            checkGameState();
-            // Then if needed, show next word
-            generateNewWord();
-            startTime = System.currentTimeMillis();
-        }
-
-        private void checkGameState() {
-            if (successCounter == 1) {
-                // gameState.setBaseHealth(100); // Restore Boat's HP
-                // gameState.setMinigamePlaysLeft(0); // Signal main game that it cannot return to the minigame (unless a PowerUp updates value)
-                game.setScreen(new MainMenuScreen(game)); // Return to the main race
-                // TODO
-                // gameState.getDeathPoint(); // Respawn at death point
-                // gameState.getMyBoat();     // Return to with previous selected Boat
-                // gameState.getRivals();     // Return Rivals to the main race
-            } else if (failCounter >= 3) {
-                dispose();
-                // game.setScreen(new GameOverScreen(game)); TODO
-            }
-        }
-
-
-
     @Override
     public void render(float delta) {
         ScreenUtils.clear(0, 0, 0.2f, 1);
@@ -159,15 +79,13 @@ public class MinigameScreen implements Screen {
         game.batch.setProjectionMatrix(camera.combined);
         stage.act();
         stage.draw();
-        // Calculates the remaining time to display real time coundown
-        remainingTime = (timeLimit - (System.currentTimeMillis() - startTime)) / 1000f;
-        if(remainingTime <= 0){
-            failCounter++;
-            checkGameState();
-            generateNewWord();
-            startTime = System.currentTimeMillis();
+        minigameLogic.run();
+        int res = minigameLogic.checkGameState();
+        if(res == 1){
+            game.setScreen(new MainMenuScreen(game));
+        }else if(res == 3){
+            dispose();
         }
-        // TODO fix visuals on screen XD
         game.batch.begin();
         // Background sceneary
         //
@@ -177,14 +95,14 @@ public class MinigameScreen implements Screen {
         game.batch.draw(textOutput, 250, 250, panelWidth, panelHeight);
         game.font.draw(game.batch,"TYPE THE WORD!!",325,400);
         // Calls the firt word to display
-        game.font.draw(game.batch, currentWord, 325, 350);
+        game.font.draw(game.batch, minigameLogic.getCurrentWord(), 325, 350);
         // Displays on real time user input
         game.batch.draw(playerInput, 250, 50, panelWidth, 80);
-        game.font.draw(game.batch, "Reply: " + typedWord, 275,100);
+        game.font.draw(game.batch, "Reply: " + minigameLogic.getTypedWord(), 275,100);
         // Displays timer countdown
         game.batch.draw(timerIcon, 725, 454, timerWidth, timerHeight);
-        game.font.draw(game.batch, String.format("[%.2f]", remainingTime), 750, 470);
-        game.font.draw(game.batch,"Remaining attempts: " + (3-failCounter),300,50);
+        game.font.draw(game.batch, String.format("[%.2f]", minigameLogic.getRemainingTime()), 750, 470);
+        game.font.draw(game.batch,"Remaining attempts: " + (3-minigameLogic.getFailCounter()),300,50);
         game.batch.end();
 
 
