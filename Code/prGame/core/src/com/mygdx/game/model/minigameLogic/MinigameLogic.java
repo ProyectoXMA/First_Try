984b6d226dad69aec4bb84140ff68464affde2ba
Angel Escaño <byanicoyt@gmail.com>
2024-05-26 22:00:45 +0200

    Update MinigameLogic.java

--- Code/prGame/core/src/com/mygdx/game/model/minigameLogic/MinigameLogic.java
+++ Code/prGame/core/src/com/mygdx/game/model/minigameLogic/MinigameLogic.java
@@ -2,7 +2,6 @@ package com.mygdx.game.model.minigameLogic;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputAdapter;
-import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.mygdx.game.GameState;
 
