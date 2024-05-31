556bb2f8064bf0c14faf49a22df2fb890532dce0
PabloHorJim <pablohorjim@uma.es>
2024-05-31 10:30:16 +0200

    Commented things about Player and PlayerOCntrolledStrategy

--- Code/prGame/core/src/com/mygdx/game/model/Player.java
+++ Code/prGame/core/src/com/mygdx/game/model/Player.java
@@ -6,7 +6,7 @@ import com.mygdx.game.model.movement.PlayerControlledStrategy;
 import com.mygdx.game.util.UserAction;
 
 /**
- * Player class will store objects important for the player thoughout the game. As this objects will be controlled by the player,
+ * Player class will store objects important for the player along the game. As this objects will be controlled by the player,
  * this class will be notified by the corresponding inputManager when the user sends and input. The player will then notify its objects.
  */
 public class Player implements InputSubscribed {
@@ -19,6 +19,12 @@ public class Player implements InputSubscribed {
     public Player(){
         this(DEFAULT_BOAT_TYPE);
     }
+
+    /**
+     * This method will set the boat of the player. As the playerBoat needs to be controlled by the player,
+     * the movement strategy of the boat will be set to PlayerControlledStrategy.
+     * @param boat the boat that will be assigned to the player (no movement strategy assigned yet)
+     */
     public void setBoat(Boat boat) {
         MovementStrategy playerControlledStrategy = new PlayerControlledStrategy();
         boat.setMovementStrategy(playerControlledStrategy);
@@ -28,8 +34,14 @@ public class Player implements InputSubscribed {
         return boat;
     }
 
+    /**
+     * The Player is subscribed to the inputManager, so it will be notified by it when the user sends an input.
+     * Then the player will redirect the input to the PlayerControlledStrategy of the boat.
+     * @param press whether the key is pressed or released
+     * @param action the action that the user has triggered (LEFT,RIGHT,UP,DOWN,ENTER,ESC)
+     */
     @Override
-    public void listen(boolean press, UserAction keycode) {
-        ((PlayerControlledStrategy) boat.getMovementStrategy()).listen(press, keycode);
+    public void listen(boolean press, UserAction action) {
+        ((PlayerControlledStrategy) boat.getMovementStrategy()).listen(press, action);
     }
 }
--- Code/prGame/core/src/com/mygdx/game/model/movement/PlayerControlledStrategy.java
+++ Code/prGame/core/src/com/mygdx/game/model/movement/PlayerControlledStrategy.java
@@ -7,17 +7,23 @@ import com.mygdx.game.util.UserAction;
 
 /**
  * Observer pattern: This class will subscribe to the event manager and will be notified by it.
+ * This class is not directly conected to the inputManager, but will be notified by the Player class
+ * about the events that the user has triggered (the Player class stores a reference to the Boat of the Player).
  */
 public class PlayerControlledStrategy implements MovementStrategy, InputSubscribed {
     private UserAction nextAction;
-    @Override
 
+    /**
+     * The movable will call this method to move itself, passing its instance to the strategy.
+     * @param movable the object that will be moved and calls this method
+     * @param delta the time elapsed since the last update
+     */
+    @Override
     public void move(Movable movable, float delta) {
-        if (nextAction == null) {
-            return;
-        }
         //TODO: I dont think we will have more PlayerControlled objects, but if we do, this should be refactored.
+        //The compute the lateral and vertical speed of the movable object. In case of boat, horizontal is the handling.
         float horizontalSpeed = movable instanceof Boat ? ((Boat) movable).getHandling() : movable.getSpeed();
+        //In any case, the vertical speed is the speed of the object.
         float verticalSpeed = movable.getSpeed();
         switch (nextAction) {
             case MOVE_LEFT:
@@ -30,10 +36,20 @@ public class PlayerControlledStrategy implements MovementStrategy, InputSubscrib
                 movable.adjustY(verticalSpeed * delta);
                 Gdx.app.log("Input","Moving to the right");
                 break;
+            default:
+                //In case no user action is detected, the object will continue moving in the same direction (upwards).
+                movable.adjustY(verticalSpeed * delta);
+                break;
         }
     }
+
+    /**
+     * This method will be called by the Player class when the user sends an input.
+     * @param press whether the key is pressed or released
+     * @param action the action that the user has triggered (LEFT,RIGHT,UP,DOWN,ENTER,ESC)
+     */
     @Override
-    public void listen(boolean press, UserAction keycode) {
-        nextAction = press? keycode : null;
+    public void listen(boolean press, UserAction action) {
+        nextAction = press? action : null;
     }
 }
