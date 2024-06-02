4a8e89898a29c07070c95e2bcfca5934bf09682a
11Manub11 <manuelbarrios@uma.es>
2024-06-02 15:42:21 +0200

    Added boat selection functionality

--- Code/prGame/core/src/com/mygdx/game/controller/MenuController.java
+++ Code/prGame/core/src/com/mygdx/game/controller/MenuController.java
@@ -161,7 +161,7 @@ public class MenuController implements Screen{
         play.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
-                game.setScreen(new minigameTutorial(game));
+                game.setScreen(new LegController(game));
             }
         });
 
